8d25cb1df4dbd65839bc4b07f5e24a68080fe404
Eduardo Cuducos <cuducos@gmail.com>
2016-06-14 20:28:50 -0300

    mkstemp(): Explicity is better than implicity

--- webassets_elm/__init__.py
+++ webassets_elm/__init__.py
@@ -31,13 +31,13 @@ class Elm(ExternalTool):
         """
 
         # write to a temp file
-        tmp = mkstemp(suffix='.js')
+        *_, tmp_path = mkstemp(suffix='.js')
         elm_make = self.binary or 'elm-make'
-        write_args = [elm_make, kw['source_path'], '--output', tmp[1], '--yes']
+        args = [elm_make, kw['source_path'], '--output', tmp_path, '--yes']
         with TemporaryFile(mode='w') as fake_write_obj:
-            self.subprocess(write_args, fake_write_obj)
+            self.subprocess(args, fake_write_obj)
 
         # read the temp file
         cat_or_type = 'type' if platform == 'win32' else 'cat'
-        read_args = [cat_or_type, tmp[1]]
+        read_args = [cat_or_type, tmp_path]
         self.subprocess(read_args, out)
