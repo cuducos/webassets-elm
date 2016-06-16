13b2f91c498caecbf66002d5faf61f447817bf91
Eduardo Cuducos <cuducos@gmail.com>
2016-06-16 00:18:31 -0300

    Better handle temporary files

--- webassets_elm/__init__.py
+++ webassets_elm/__init__.py
@@ -1,5 +1,5 @@
 from sys import platform
-from tempfile import mkstemp, TemporaryFile
+from tempfile import NamedTemporaryFile, TemporaryFile
 from webassets.filter import ExternalTool
 
 __all__ = ('Elm')
@@ -31,13 +31,15 @@ class Elm(ExternalTool):
         """
 
         # write to a temp file
-        tmp = mkstemp(suffix='.js')
+        tmp = NamedTemporaryFile(suffix='.js', delete=True)
         elm_make = self.binary or 'elm-make'
-        write_args = [elm_make, kw['source_path'], '--output', tmp[1], '--yes']
+        source = kw['source_path']
+        write_args = [elm_make, source, '--output', tmp.name, '--yes']
         with TemporaryFile(mode='w') as fake_write_obj:
             self.subprocess(write_args, fake_write_obj)
 
         # read the temp file
         cat_or_type = 'type' if platform == 'win32' else 'cat'
-        read_args = [cat_or_type, tmp[1]]
+        read_args = [cat_or_type, tmp.name]
         self.subprocess(read_args, out)
+        tmp.close()
