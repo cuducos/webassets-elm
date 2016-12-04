a4fb0c09665b5a3f347853bbadcbaecf67097d4c
Stefan Zimmermann <zimmermann.code@gmail.com>
2016-12-05 00:39:44 +0100

    Run elm-package install before elm-make

--- webassets_elm/__init__.py
+++ webassets_elm/__init__.py
@@ -17,10 +17,17 @@ class Elm(ExternalTool):
         The path to the ``elm-make`` binary. If not set, assumes ``elm-make``
         is in the system path.
 
+    ELM_PACKAGE_BIN
+        The path to the ``elm-package`` binary. If not set, assumes
+        ``elm-package`` is in the system path.
+
     """
 
     name = 'elm'
-    options = {'binary': 'ELM_MAKE_BIN'}
+    options = {
+        'elm_make_binary': 'ELM_MAKE_BIN',
+        'elm_package_binary': 'ELM_PACKAGE_BIN',
+    }
     max_debug_level = None
 
     def input(self, _in, out, **kw):
@@ -31,12 +38,19 @@ class Elm(ExternalTool):
         output to stdout.
         """
 
+        # first install/update elm-package dependencies
+        elm_package = self.elm_package_binary or 'elm-package'
+        install_args = [elm_package, 'install', '--yes']
+        # set utf-8 to handle unicode bullet point symbols
+        with TemporaryFile(mode='w', encoding='utf-8') as fake_write_obj:
+            self.subprocess(install_args, fake_write_obj)
+
         # create a temp file
         tmp = NamedTemporaryFile(suffix='.js', delete=False)
         tmp.close()  # close it so windows can read it
 
         # write to a temp file
-        elm_make = self.binary or 'elm-make'
+        elm_make = self.elm_make_binary or 'elm-make'
         source = kw['source_path']
         write_args = [elm_make, source, '--output', tmp.name, '--yes']
         with TemporaryFile(mode='w') as fake_write_obj:
