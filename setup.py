266ff951ba6cda1d92b373b35987061d0ad62b81
Eduardo Cuducos <cuducos@gmail.com>
2017-01-26 19:52:09 -0200

    Add support/tests for Python 3.6

--- setup.py
+++ setup.py
@@ -15,7 +15,8 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5'
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
     ],
     description='Elm filter for webassets',
     install_requires=['webassets'],
