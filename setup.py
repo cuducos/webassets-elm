2b56ae4744ae8bb3ace9f05ca3351f8920794e86
Eduardo Cuducos <cuducos@gmail.com>
2017-01-27 13:02:53 -0200

    Remove support for Python 2

--- setup.py
+++ setup.py
@@ -12,7 +12,6 @@ setup(
         'Framework :: Flask',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
@@ -28,6 +27,6 @@ setup(
     test_suite='nose.collector',
     tests_require=['nose'],
     url=REPO_URL,
-    version='0.1.5',
+    version='0.1.6',
     zip_safe=False
 )
