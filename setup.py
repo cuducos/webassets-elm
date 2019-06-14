1ade0dd81d2e6b97626189ae9f167be3704873fd
Martijn Pieters <mj@zopatista.com>
2019-06-14 14:57:13 +0100

    Simpler: drop support for 3.3
    3.3 is way beyond its EOL and there is simply no support for it anymore in testing tools. Rather than hunt for what package versions might still work, just drop it altogether.

--- setup.py
+++ setup.py
@@ -12,7 +12,6 @@ with open('README.rst') as readme:
             'Framework :: Flask',
             'Intended Audience :: Developers',
             'License :: OSI Approved :: MIT License',
-            'Programming Language :: Python :: 3.3',
             'Programming Language :: Python :: 3.4',
             'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6'
